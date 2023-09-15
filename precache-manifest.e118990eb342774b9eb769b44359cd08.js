9418b8057771ac00b49ab53f2168ed0e1466c917
Srishashank2003 <dsrishashank308@gmail.com>
2023-09-15 21:10:09 +0530

    Updates

---
+++ precache-manifest.e118990eb342774b9eb769b44359cd08.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b33fc2fca396a9ed14bf4bad34a7a674",
+    "url": "/Weather-Vista/index.html"
+  },
+  {
+    "revision": "2f6f7b018277e780887f",
+    "url": "/Weather-Vista/static/css/main.db26e666.chunk.css"
+  },
+  {
+    "revision": "7f3b2d20633ad4703c0a",
+    "url": "/Weather-Vista/static/js/2.f15878e7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Weather-Vista/static/js/2.f15878e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f6f7b018277e780887f",
+    "url": "/Weather-Vista/static/js/main.87475df3.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/Weather-Vista/static/js/main.87475df3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "847f8c4d7a54a1d342fe",
+    "url": "/Weather-Vista/static/js/runtime-main.6d184886.js"
+  },
+  {
+    "revision": "0a218296b39a3569fe9c933e3ababb49",
+    "url": "/Weather-Vista/static/media/WeatherIcons.0a218296.gif"
+  },
+  {
+    "revision": "75e0f8ca422777deab777d3abe0a90e7",
+    "url": "/Weather-Vista/static/media/background.75e0f8ca.jpg"
+  },
+  {
+    "revision": "e1698332435d6a6fb48294f85157aeab",
+    "url": "/Weather-Vista/static/media/city.e1698332.jpg"
+  }
+]);
